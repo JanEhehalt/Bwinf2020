68050ba4f8bf1ec4e6887fe231fd41885a6edd48
Jan Ehehalt <GammelJAN>
2020-09-30 08:28:46 +0200

    mngoisrdoiugh

--- a5.java
+++ a5.java
@@ -19,3 +19,44 @@ class Main{
         }
     }
 }
+
+/*
+
+
+class Student{
+	boolean gifted;
+	int metWish;
+	int[] wishes = new int[3];
+	public schueler(int wishOne, int wishTwo, int wishThree){
+		wishes[0] = wishOne;
+		wishes[1] = wishTwo;
+		wishes[2] = wishThree;
+	}
+}
+
+// ERSTWÜNSCHE VERTEILEN
+static void geschenke(int n){
+	for(int i = 0; i < students.length; i++){
+		if(students[i].gifted) continue;
+		if(presents[student.wishes[n]] = true) continue;
+		else{
+			student.gifted = true;
+			student.metWish = n;
+			presents[student.wishes[n]] = true;
+		}
+	}
+}
+static void nachverteilung(){
+	for(int i = 0; i < students.length; i++){
+		if(students[i].gifted) continue;
+		else 
+			for(int n = 0; n < presents.length; n++){
+				if(!presents[i]){
+					student.gifted = true;
+					student.metWish = -1;
+					presents[n] = true;
+				}
+			}
+	}
+}
+*/
