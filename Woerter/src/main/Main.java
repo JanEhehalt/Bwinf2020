ace0fbac6d059890b4260d0e94d14bcefad92805
GammelJAN <jan.ehehalt@gmail.com>
2020-11-02 21:50:26 +0100

    alles kacke

--- Woerter/src/main/Main.java
+++ Woerter/src/main/Main.java
@@ -1,5 +1,27 @@
-package main;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+	
 
+    public static void main(String[] args){
+		try {
+	    	File data = new File("src/raetsel0.txt");
+	        //File data = new File(args[0]);
+	        Scanner sc = new  Scanner(data);
+	
+	        ArrayList<String> words;
+	        ArrayList<String> sentence;
+	        
+	        int currentChar;
+	        for(sc.)
+	        
+	        sc.close();
+		}
+		catch(FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 }
