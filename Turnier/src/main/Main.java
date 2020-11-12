9585fc9c00fda624391eee25274d0d04f7b7d80c
Jonathan Hager <jonathan.hager@protonmail.com>
2020-11-12 18:46:46 +0100

    ölkj

--- Turnier/src/main/Main.java
+++ Turnier/src/main/Main.java
@@ -1,10 +1,11 @@
+package main;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+
 class Main{
-	
-	
     public static void main(String[] args){
     	try {
     		
