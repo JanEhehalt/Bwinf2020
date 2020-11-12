3c4423cac1bfc72e8fc877d82f1b126d5c94771d
GammelJAN <jan.ehehalt@gmail.com>
2020-11-12 18:43:17 +0100

    lol

---
+++ Turnier/src/main/Main.java
@@ -0,0 +1,16 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+class Main{
+	
+	
+    public static void main(String[] args){
+    	try {
+    		
+    	}
+    	catch(FileNotFoundException e) {
+    		e.printStackTrace();
+    	}
+    }
+}
