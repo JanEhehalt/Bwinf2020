aec045991ef2d15d4372c5f2962fbdfc5704cf9d
GammelJAN <jan.ehehalt@gmail.com>
2020-11-13 18:22:45 +0100

    lol

--- Turnier/src/main/Player.java
+++ Turnier/src/main/Player.java
@@ -1,5 +1,12 @@
 package main;
 
 public class Player {
-
+	public int id;
+	public int strength;
+	public int wins;
+	Player(int id, int strength){
+		this.strength = strength;
+		this.wins = 0;
+		this.id = id;
+	}
 }
