f7770ef9cd22958129da432449637dbf656959ed
GammelJAN <jan.ehehalt@gmail.com>
2020-11-14 11:36:10 +0100

    igsierdgjis

---
+++ Streichholz/src/Match.java
@@ -0,0 +1,13 @@
+import java.util.Objects;
+
+public class Match {
+	int vx;
+	int vy;
+	int rotation;
+	
+	public Match(int vx, int vy, int rotation) {
+		this.vx = vx;
+		this.vy = vy;
+		this.rotation = rotation;
+	}
+}
