638429b01e4be3174e8c4e5ec9ecfa769323a976
GammelJAN <jan.ehehalt@gmail.com>
2020-11-14 12:20:50 +0100

    Vektorkacke

--- Streichholz/src/Main.java
+++ Streichholz/src/Main.java
@@ -2,6 +2,17 @@ import java.util.ArrayList;
 
 public class Main {
 	public static void main(String[] args) {
+		ArrayList<Match> matches = getExample();;
+		
+	}
+	
+	public static ArrayList<Match> getExample(){
 		ArrayList<Match> matches = new ArrayList<>();
+		matches.add(new Match(1,1,6));
+		System.out.println(matches.get(0).x);
+		System.out.println(matches.get(0).y);
+		System.out.println(matches.get(0).vx);
+		System.out.println(matches.get(0).vy);
+		return matches;
 	}
 }
--- Streichholz/src/Match.java
+++ Streichholz/src/Match.java
@@ -1,13 +1,19 @@
 import java.util.Objects;
 
 public class Match {
-	int vx;
-	int vy;
+	int x;
+	int y;
+	double vx;
+	double vy;
 	int rotation;
+	int length;
 	
-	public Match(int vx, int vy, int rotation) {
-		this.vx = vx;
-		this.vy = vy;
-		this.rotation = rotation;
+	public Match(int x, int y, int rotation) {
+		this.x = x;
+		this.y = y;
+		this.rotation = rotation * 30;
+		this.length = 5;
+		this.vx = Math.cos(Math.toRadians(rotation))*length;
+		this.vy = Math.sin(Math.toRadians(rotation))*length;
 	}
 }
